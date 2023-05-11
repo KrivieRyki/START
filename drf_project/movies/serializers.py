ee316dfb8824e2f05df5db2b3f09dd45af5fb67a
Max Moroz <mi_morozov@slxg.ru>
2023-05-11 11:22:08 +0300

    the end

--- drf_project/movies/serializers.py
+++ drf_project/movies/serializers.py
@@ -1,12 +1,17 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from movies.models import Movie
 
 
 class MovieSerializer(ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='movie-detail',
+        lookup_field='pk'
+    )
     class Meta:
         model = Movie
-        fields = "__all__"
+        fields = ('url', 'title', 'genre', 'year')
         read_only_fields = (
             "id",
             "created_date",
